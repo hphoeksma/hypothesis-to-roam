2c1f35b26a5c201976aacdeb80ce17d42a3b82bf
Henjo Hoeksma <henjo@upassist.com>
2021-02-03 17:52:26 +0100

    Fetching articles

--- src/components/Process.js
+++ src/components/Process.js
@@ -1,24 +1,22 @@
-import React, {Fragment, useState, useRef} from 'react'
+import React, {Fragment, useRef, useState} from 'react'
 import {api} from '../api/api'
 import './Process.scss'
 import moment from 'moment'
 import {toast} from 'react-toastify'
 import {Link} from 'react-router-dom'
-import {
-    FaTrashAlt,
-    HiOutlineClipboardCopy,
-    ImSpinner8,
-    IoArrowRedoOutline,
-} from 'react-icons/all'
+import {FaTrashAlt, HiOutlineClipboardCopy, ImSpinner8, IoArrowRedoOutline,} from 'react-icons/all'
 import {defaultTemplate} from '../App'
 import LinkToHypothesis from '../decorators/LinkToHypothesis'
+import TurndownService from 'turndown'
 
 export default function Process(props) {
     const uri = props.location.search.split('=')[1]
     const [annotations, setAnnotations] = useState(null)
+    const [article, setArticle] = useState(null)
     const template = localStorage.getItem('template') ? localStorage.getItem('template') : defaultTemplate
     const textarea = useRef(null)
     const title = useRef(null)
+    const articleText = useRef(null)
     const tagJoin = localStorage.getItem('tagjoin') ? localStorage.getItem('tagjoin') : ' '
 
     if (!annotations) {
@@ -90,12 +88,35 @@ export default function Process(props) {
     const copyTemplate = () => {
         textarea.current.select()
         document.execCommand('copy')
-        toast.info(`🚀 The content is copied, paste it in your Roam! ${<Link to="/">Go home</Link>}`)
+        toast.info(`🚀 The content is copied, paste it in your Roam!`)
+    }
+
+    const copyArticle = () => {
+        articleText.current.select()
+        document.execCommand('copy')
+        toast.info(`🚀 The content is copied, paste it in your Roam!`)
+    }
+
+    const fetchArticle = () => {
+        const turndownService = new TurndownService()
+        fetch(uri).then(response => response.text())
+            .then(data => {
+                // Convert the HTML string into a document object
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(data, 'text/html');
+                const markdown = turndownService.turndown(doc.querySelector('article'));
+                if (markdown) {
+                    setArticle(markdown)
+                }
+
+            }).catch(function (err) {
+            // There was an error
+            console.warn('Something went wrong.', err);
+        });
     }
 
     const cleanupHypothesis = () => {
         annotations.forEach(annotation => {
-            console.log(annotation.id)
             api.deleteAnnotation(annotation.id).then(result => {
                 if (result === true) {
                     toast.success('👍🏻 Annotation removed')
@@ -104,15 +125,21 @@ export default function Process(props) {
         })
     }
 
+    // Fetch the article contents
+    fetchArticle();
+
     return (
         <Fragment>
-            {!annotations ? <p className={'has-loader'}><ImSpinner8 className={'loading'} /> Preparing annotations, please hold... (it won't take long 😎)</p> :
+            {!annotations ?
+                <p className={'has-loader'}><ImSpinner8 className={'loading'} /> Preparing annotations, please hold...
+                                                                                 (it won't take long 😎)</p> :
                 <div className={'process'}>
                     <h2><span>Copy your content</span><LinkToHypothesis uri={uri} prefix={true} /></h2>
                     <h3>Title</h3>
                     <div className="textarea-wrapper">
                         <textarea ref={title} defaultValue={annotations[0].document.title[0]}></textarea>
-                        <button onClick={copyTitle} title={'Copy the title'} className={'button button--plain'}><HiOutlineClipboardCopy/></button>
+                        <button onClick={copyTitle} title={'Copy the title'} className={'button button--plain'}>
+                            <HiOutlineClipboardCopy /></button>
                     </div>
                     <h3>Contents</h3>
                     <div className="textarea-wrapper">
@@ -128,11 +155,26 @@ export default function Process(props) {
                                     .replace('{highlights}', getAnnotations('highlights'))
                                     .replace('{allTags}', getAnnotations('allTags'))
                             }></textarea>
-                        <button onClick={copyTemplate} title={'Copy the contents'} className={'button button--plain'}><HiOutlineClipboardCopy/></button>
+                        <button onClick={copyTemplate} title={'Copy the contents'} className={'button button--plain'}>
+                            <HiOutlineClipboardCopy /></button>
+                    </div>
+                    <h3>Article <span className="experimental">Experimental</span></h3>
+                    <p className="note">We attempt to fetch the contents of the article and convert it to Markdown. If succeeded, it will
+                       show up here.<br />
+                       Please be aware that copying large amounts of texts into Roam may lead to a freeze of Roam.</p>
+                    <div className="textarea-wrapper">
+                        <textarea
+                            className={'display-linebreak'}
+                            ref={articleText}
+                            defaultValue={article}></textarea>
+                        <button onClick={copyArticle} title={'Copy the contents'} className={'button button--plain'}>
+                            <HiOutlineClipboardCopy /></button>
                     </div>
                     <h2>
                         <span>Finished? Clean up hypothes.is if you like...</span>
-                        <button onClick={cleanupHypothesis} className={'button button--warning button--inline'}><FaTrashAlt /> Remove annotations from hypothes.is</button>
+                        <button onClick={cleanupHypothesis} className={'button button--warning button--inline'}>
+                            <FaTrashAlt /> Remove annotations from hypothes.is
+                        </button>
                     </h2>
                     <Link to={'/'} className={'back'} title={'Go back'}><IoArrowRedoOutline /> Go back</Link>
                 </div>
